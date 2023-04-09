6424c939e4362395b8ec3258683c421b1c377cad
ryanrcahill3 <rrcahill@crimson.ua.edu>
2023-04-08 19:09:54 -0500

    added cors

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("OpenPolicy",
+    builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,7 +30,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+
+
 app.UseAuthorization();
+
+app.UseCors("OpenPolicy");
 
 app.MapControllers();
 
