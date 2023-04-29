52c93154fe37809b512ff584b4b0905469636d4b
ryanrcahill3 <rrcahill@crimson.ua.edu>
2023-04-28 21:03:46 -0500

    transactions

--- database/SaveClothing.cs
+++ database/SaveClothing.cs
@@ -14,20 +14,18 @@ namespace api.database
             using var con = new MySqlConnection(cs);
             con.Open();
 
-            string stm = @"INSERT INTO clothing(title, type, occasion, size, image) VALUES(@title, @type, @occasion, @size, @image)";
+            string stm = @"INSERT INTO clothing(title, size, imageURL, userID) VALUES(@title, @size, @image, @userID)";
 
             using var cmd = new MySqlCommand(stm, con);
 
             cmd.Parameters.AddWithValue("@title", myClothing.Title);
             System.Console.WriteLine(myClothing.Title);
-            cmd.Parameters.AddWithValue("@type", myClothing.Type);
-            System.Console.WriteLine(myClothing.Type);
-            cmd.Parameters.AddWithValue("@occasion", myClothing.Occasion);
-            System.Console.WriteLine(myClothing.Occasion);
             cmd.Parameters.AddWithValue("@size", myClothing.Size);
             System.Console.WriteLine(myClothing.Size);
-            cmd.Parameters.AddWithValue("@image", myClothing.ImageURL);
+            cmd.Parameters.AddWithValue("@imageURL", myClothing.ImageURL);
             System.Console.WriteLine(myClothing.ImageURL);
+            cmd.Parameters.AddWithValue("@userID", myClothing.UserID);
+            System.Console.WriteLine(myClothing.UserID);
 
             cmd.Prepare();
 
